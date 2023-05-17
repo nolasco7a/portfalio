4a7198dcce242e6fd859c405db70004434b86f6f
Allan Nolasco <allan.nolasco@paicbd.com>
2023-05-16 23:57:30 -0500

    portfolio update

--- src/views/MyWork.js
+++ src/views/MyWork.js
@@ -8,7 +8,15 @@ export const MyWork = () => {
      <div className="main_container">
       <div className='view-margin'>
         <h1 className='title'>My Work</h1>
-        <p className='information'>Below are some of the last projects in which I have worked focused on solving the problem or challenge posed, throughout my career I have been exposed to various environments or types of industry and ultimately focused on the area of e-commerce.</p>
+        <p className='information'>Below are some of the most outstanding projects I have worked on, representing only a sample of my work. It is worth mentioning that, in addition to these showcased projects, I have also participated in other complex challenges that are not of a public nature. These additional projects include the implementation of inventory management and/or user management portals, which have been of vital importance for the success of several organizations.</p>
+
+        <p className='information'>
+          These aforementioned projects are just a representative sample of my experience and skills in developing complex and customized solutions. Throughout my career, I have worked on a number of private projects that have enabled organizations to optimize their operations and achieve outstanding results. While I cannot disclose specific details due to confidentiality agreements, I am available to discuss my contributions and additional experiences in these fields in a more private setting or during a personal interview.
+        </p>
+
+        <p>
+
+        </p>
         {
          projects.map((project, i) =>
           (i+1)%2===0
